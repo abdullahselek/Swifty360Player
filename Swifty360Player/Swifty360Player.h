30196bce5865f5cbe2d51f527e651dacd8d502fe
Abdullah Selek <abdullahselek@gmail.com>
2017-10-12 11:01:51 +0200

    create empty project

---
+++ Swifty360Player/Swifty360Player.h
@@ -0,0 +1,19 @@
+//
+//  Swifty360Player.h
+//  Swifty360Player
+//
+//  Created by Selek, Abdullah on 12.10.17.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swifty360Player.
+FOUNDATION_EXPORT double Swifty360PlayerVersionNumber;
+
+//! Project version string for Swifty360Player.
+FOUNDATION_EXPORT const unsigned char Swifty360PlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swifty360Player/PublicHeader.h>
+
+

07d6b6683094d40e367128dc17a1f375d7bb7934
Abdullah Selek <abdullahselek@gmail.com>
2017-10-20 11:40:57 +0200

    start implementing scene

---
+++ Swifty360Player/Float++.swift
@@ -0,0 +1,25 @@
+//
+//  Float++.swift
+//  Swifty360Player
+//
+//  Created by Selek, Abdullah on 20.10.17.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+import UIKit
+
+extension Float {
+
+    func getCGFloat() -> CGFloat {
+        return CGFloat(self)
+    }
+
+}
+
+extension CGFloat {
+
+    func getFloat() -> Float {
+        return Float(self)
+    }
+
+}
--- Swifty360Player/Swifty360PlayerScene.swift
+++ Swifty360Player/Swifty360PlayerScene.swift
@@ -23,23 +23,53 @@
 //  SOFTWARE.
 
 import SceneKit
+import SpriteKit
 import AVFoundation
 
 open class Swifty360PlayerScene: SCNScene {
 
     open let camera = SCNCamera()
     private var videoPlaybackIsPaused: Bool!
-    private var cameraNode: SCNNode!
+    private var cameraNode: SCNNode! {
+        let cameraNode = SCNNode()
+        cameraNode.camera = camera
+        cameraNode.position = SCNVector3Make(0.0, 0.0, 0.0)
+        return cameraNode
+    }
     private var player: AVPlayer!
 
     public init(withAVPlayer player: AVPlayer, view: SCNView) {
         super.init()
         self.videoPlaybackIsPaused = true
         self.player = player
+        self.rootNode.addChildNode(self.cameraNode)
+
     }
 
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    internal func getScene() -> SKScene {
+        let assetTrack = player.currentItem?.asset.tracks(withMediaType: .video).first
+        let assetDimensions = assetTrack != nil ? __CGSizeApplyAffineTransform(assetTrack!.naturalSize, assetTrack!.preferredTransform) :
+            CGSize(width: 1280.0, height: 1280.0)
+        let scene = SKScene(size: CGSize(width: fabsf(assetDimensions.width.getFloat()).getCGFloat(),
+                                         height: fabsf(assetDimensions.height.getFloat()).getCGFloat()))
+        scene.shouldRasterize = true
+        scene.scaleMode = .aspectFit
+        scene.addChild(getVideoNode(withPlayer: player, scene: scene))
+        return scene
+    }
+
+    internal func getVideoNode(withPlayer player: AVPlayer, scene: SKScene) -> SwiftySKVideoNode {
+        let videoNode = SwiftySKVideoNode(avPlayer: player)
+        videoNode.position = CGPoint(x: scene.size.width / 2, y: scene.size.height / 2)
+        videoNode.size = scene.size
+        videoNode.yScale = -1
+        videoNode.xScale = -1
+        videoNode.swiftyDelegate = self
+        return videoNode
     }
 
 }

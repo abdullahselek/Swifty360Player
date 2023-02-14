ba2afdbcb53e06926031defe5c63d99a58d9bd2d
Abdullah Selek <abdullahselek@gmail.com>
2023-02-14 21:22:32 +0000

    Add missing import.

--- Swifty360Player/Swifty360MotionManagerObserverItem.swift
+++ Swifty360Player/Swifty360MotionManagerObserverItem.swift
@@ -22,6 +22,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
+import Foundation
+
 internal class Swifty360MotionManagerObserverItem {
 
     internal let token: UUID
